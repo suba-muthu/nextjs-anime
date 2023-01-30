ed3e063f9a2821807ca85cc183d50be26697f045
suba <subalakshmimuthu02@gmail.com>
2023-01-30 14:35:09 +0530

    add MUI and create anime card

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import AnimeCard from '../components/animeCard'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      hello 
+      <AnimeCard /> 
     </div>
   )
 }
